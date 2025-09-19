3aaab6218656878c6fba0ae115046f810b091a96
igorkll <frerther@gmail.com>
2025-09-19 21:48:08 +0000

    Update syslbuild.py

--- syslbuild.py
+++ syslbuild.py
@@ -10,8 +10,9 @@ def require_root():
         sys.exit(os.system("sudo {} {}".format(sys.executable, " ".join(sys.argv))))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Пример получения аргумента")
-    parser.add_argument("--arch", type=str, required=True, help="the processor architecture for which the build will be made")
+    parser = argparse.ArgumentParser(description="an assembly system for creating Linux distributions. it is focused on embedded distributions")
+    parser.add_argument("--arch", choices=["amd64", "i386", "arm64", "armhf", "armel"], type=str, required=True, help="the processor architecture for which the build will be made")
+    parser.add_argument("json-path", type=str, help="the path to the json file of the project")
     args = parser.parse_args()
     
     require_root()
