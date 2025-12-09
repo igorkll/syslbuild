ff73bd05797433330097b65f1e114298160c092c
igorkll <frerther@gmail.com>
2025-12-10 00:25:46 +0300

    backup

--- projects/skyOS/hooks/customize-1.sh
+++ projects/skyOS/hooks/customize-1.sh
@@ -17,6 +17,6 @@ chmod 0644 "$1/etc/systemd/system/shell.service"
 run_in_chroot "$1" hooks/chroot/apply_settings.sh
 run_in_chroot "$1" hooks/chroot/dependencies.sh
 run_in_chroot "$1" hooks/chroot/boot_logo.sh
-# run_in_chroot "$1" hooks/chroot/disable_trash.sh
+run_in_chroot "$1" hooks/chroot/disable_trash.sh
 run_in_chroot "$1" hooks/chroot/register_shell.sh
 # run_in_chroot "$1" hooks/chroot/cleanup.sh
