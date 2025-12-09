7a07b1e7ccacc5bb2d7e077a45f6b86f05cce802
igorkll <frerther@gmail.com>
2025-12-09 20:39:42 +0300

    Update customize-1.sh

--- projects/skyOS/hooks/customize-1.sh
+++ projects/skyOS/hooks/customize-1.sh
@@ -2,11 +2,6 @@ run_in_chroot() {
     local CHROOT_PATH="$1"
     local SCRIPT_PATH="$2"
 
-    if [[ -z "$CHROOT_PATH" || -z "$SCRIPT_PATH" ]]; then
-        echo "Usage: run_in_chroot <chroot_path> <script_path>"
-        return 1
-    fi
-
     cp "$SCRIPT_PATH" "$CHROOT_PATH/.build_script" || return 1
     chmod +x "$CHROOT_PATH/.build_script" || return 1
     chroot "$CHROOT_PATH" /bin/bash /.build_script || return 1
