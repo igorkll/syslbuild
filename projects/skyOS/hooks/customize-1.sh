f053ac8e19441c1e3c4eebf536b397eb7b66ed1c
igorkll <frerther@gmail.com>
2025-12-09 21:03:52 +0300

    backup

--- projects/skyOS/hooks/customize-1.sh
+++ projects/skyOS/hooks/customize-1.sh
@@ -8,5 +8,5 @@ run_in_chroot() {
     rm -f "$CHROOT_PATH/.build_script"
 }
 
-run_in_chroot "$1" chroot/dependencies.sh
-run_in_chroot "$1" chroot/boot_logo.sh
+run_in_chroot "$1" hooks/chroot/dependencies.sh
+run_in_chroot "$1" hooks/chroot/boot_logo.sh
