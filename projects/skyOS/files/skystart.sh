22e1a68cd33c7a044438ab16cb0bc59e3cdb7371
igorkll <frerther@gmail.com>
2025-12-11 21:39:57 +0300

    backup

--- projects/skyOS/files/skystart.sh
+++ projects/skyOS/files/skystart.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# ------------- parse kernel arguments
+
+sky_debug=$(cat /proc/cmdline | tr ' ' '\n' | grep '^sky_debug=' | cut -d= -f2)
+sky_bash=$(cat /proc/cmdline | tr ' ' '\n' | grep '^sky_bash=' | cut -d= -f2)
+
+if [ "$sky_debug" = "true" ]; then
+    plymouth quit
+    if [ "$sky_bash" = "true" ]; then
+        bash
+    fi
+else
+    exec >/dev/null 2>&1
+fi
+
+# ------------- initialization
+
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 mkdir -p $XDG_RUNTIME_DIR
 chmod 700 $XDG_RUNTIME_DIR
@@ -44,8 +61,10 @@ else
 fi
 
 # ------------- run shell
-plymouth quit
-sleep 3
+
+if [ "$skydebug_enabled" != "true" ]; then
+    plymouth quit
+fi
 
 electron --ozone-platform=wayland --enable-features=UseOzonePlatform,WaylandWindowDecorations --no-sandbox /sky/system/ElectronApplication
 bash
