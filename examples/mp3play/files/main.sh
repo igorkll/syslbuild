5d7bcf9b14f18133e4e638e70dbd1ad1737670c4
igorkll <frerther@gmail.com>
2026-02-01 01:12:23 +0300

    Update main.sh

--- examples/mp3play/files/main.sh
+++ examples/mp3play/files/main.sh
@@ -6,7 +6,7 @@ export XDG_RUNTIME_DIR=/run/user/0
 mkdir -p $XDG_RUNTIME_DIR
 chmod 700 $XDG_RUNTIME_DIR
 
-pulseaudio --start --exit-idle-time=-1
+pulseaudio --system --start --exit-idle-time=-1
 
 while true; do
     mpg123 -o pulse /sound.mp3
