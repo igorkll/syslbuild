47ff368d200fa9c3478aa4aecd58c74d05fb9072
igorkll <frerther@gmail.com>
2026-02-01 01:39:46 +0300

    backup

--- examples/mp3play/chroot/cleanup.sh
+++ examples/mp3play/chroot/cleanup.sh
@@ -21,27 +21,3 @@ rm -rf /usr/share/systemd
 rm -rf /usr/lib/systemd
 rm -rf /var/lib/systemd
 rm -rf /etc/systemd
-
-# remove user system
-rm -f /etc/shadow
-rm -f /etc/gshadow
-rm -f /etc/passwd
-rm -f /etc/group
-rm -f /etc/shadow-
-rm -f /etc/gshadow-
-rm -f /etc/passwd-
-rm -f /etc/group-
-rm -f /usr/sbin/getty
-rm -f /usr/sbin/agetty
-rm -f /usr/sbin/adduser
-rm -f /usr/sbin/addgroup
-rm -f /usr/sbin/deluser
-rm -f /usr/sbin/delgroup
-rm -f /usr/sbin/useradd
-rm -f /usr/sbin/usermod
-rm -f /usr/sbin/userdel
-rm -f /usr/sbin/pwck
-rm -f /usr/sbin/pwconv
-rm -f /etc/login.defs
-rm -f /etc/default/useradd
-rm -rf /etc/skel
--- examples/mp3play/files/main.sh
+++ examples/mp3play/files/main.sh
@@ -6,7 +6,12 @@ export XDG_RUNTIME_DIR=/run/user/0
 mkdir -p $XDG_RUNTIME_DIR
 chmod 700 $XDG_RUNTIME_DIR
 
-pulseaudio --system --start --exit-idle-time=-1
+mount -t tmpfs -o "nodev,nosuid,size=${RUNSIZE:-10%},mode=1777" tmpfs /var
+mount -t tmpfs -o "nodev,nosuid,size=${RUNSIZE:-10%},mode=1777" tmpfs /root
+
+pulseaudio --daemonize=yes --exit-idle-time=-1
+
+bash
 
 while true; do
     mpg123 -o pulse /sound.mp3
