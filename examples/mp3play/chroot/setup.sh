5075df9190e337383dc88fae7696e173e0541fca
igorkll <frerther@gmail.com>
2026-02-01 02:47:44 +0300

    backup

--- examples/mp3play/chroot/setup.sh
+++ examples/mp3play/chroot/setup.sh
@@ -3,6 +3,9 @@ usermod -s /bin/bash root
 
 useradd -s /usr/sbin/nologin mp3player
 
-systemctl enable mp3player.service
+systemctl enable mp3player
+systemctl enable dbus
+
+loginctl enable-linger mp3player
 
 update-initramfs -u
--- examples/mp3play/files/main.sh
+++ examples/mp3play/files/main.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-bash
-
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
-mkdir -p "$XDG_RUNTIME_DIR"
-chmod 700 "$XDG_RUNTIME_DIR"
-
 pulseaudio --daemonize=yes --exit-idle-time=-1
 
 while true; do
