6759d4b175a419d652d0c4c0021ebb70d86512c5
igorkll <frerther@gmail.com>
2026-02-01 04:31:13 +0300

    Update setup.sh

--- examples/mp3play/chroot/setup.sh
+++ examples/mp3play/chroot/setup.sh
@@ -1,17 +1,19 @@
+# set root password (for debug)
 echo "root:root" | chpasswd
 usermod -s /bin/bash root
 
-useradd -s /usr/sbin/nologin mp3player
-
+# system lockdown
 systemctl mask getty@tty2.service
 systemctl mask getty@tty3.service
 systemctl mask getty@tty4.service
 systemctl mask getty@tty5.service
 systemctl mask getty@tty6.service
 
+# create mp3player user
+useradd -m -s /usr/sbin/nologin mp3player
 systemctl enable mp3player
 systemctl enable dbus
-
 loginctl enable-linger mp3player
 
+# update init ram disk
 update-initramfs -u
