b8ba11f9d191d8e2fdb9f39025fa1cb5f7287cbc
igorkll <frerther@gmail.com>
2026-02-01 04:50:58 +0300

    Update setup.sh

--- examples/mp3play/chroot/setup.sh
+++ examples/mp3play/chroot/setup.sh
@@ -9,8 +9,18 @@ systemctl mask getty@tty4.service
 systemctl mask getty@tty5.service
 systemctl mask getty@tty6.service
 
+systemctl disable getty@tty2.service
+systemctl disable getty@tty3.service
+systemctl disable getty@tty4.service
+systemctl disable getty@tty5.service
+systemctl disable getty@tty6.service
+
 # create mp3player user
-useradd -m -s /usr/sbin/nologin mp3player
+useradd -s /usr/sbin/nologin mp3player
+mkdir -p /home/mp3player
+chown -R mp3player:mp3player /home/mp3player
+chmod -R 0700 /home/mp3player
+
 systemctl enable mp3player
 systemctl enable dbus
 loginctl enable-linger mp3player
