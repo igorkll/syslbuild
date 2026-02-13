d81869a4540784358ea5d89fe60c5c2eaa1d298e
igorkll <frerther@gmail.com>
2026-02-13 18:00:43 +0300

    Update gnuboxmaker.py

--- gnuboxmaker.py
+++ gnuboxmaker.py
@@ -815,12 +815,20 @@ def load_project(path):
         with open(runshell_path, "w") as f:
             f.write(f"""#!/bin/bash
 
+# gnubox maker tty app example
+
 # disable tty hotkeys
 stty intr undef   # Ctrl+C
 stty quit undef   # Ctrl+\\
 stty stop undef   # Ctrl+S
 stty start undef  # Ctrl+Q
 stty susp undef   # Ctrl+Z
+
+# disable echo mode
+stty -echo
+
+# set cursor to first line
+clear
 
 while true; do
     echo test
