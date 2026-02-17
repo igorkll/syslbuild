1a2d277e72af221f2060af390f23516838af184b
igorkll <frerther@gmail.com>
2026-02-17 22:50:24 +0300

    backup

--- gnuboxmaker_examples/electron_wayland_bashtest/resources/runshell.sh
+++ gnuboxmaker_examples/electron_wayland_bashtest/resources/runshell.sh
@@ -4,3 +4,4 @@ systemctl --user enable pipewire pipewire-pulse wireplumber
 systemctl --user start pipewire pipewire-pulse wireplumber
 
 bash
+electron /ElectronApplication --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --no-sandbox
