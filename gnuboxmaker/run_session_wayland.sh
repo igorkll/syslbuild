81cbb858cebb7f23247d390b370709e4808de571
igorkll <frerther@gmail.com>
2026-02-19 01:32:19 +0300

    fix

--- gnuboxmaker/run_session_wayland.sh
+++ gnuboxmaker/run_session_wayland.sh
@@ -2,14 +2,14 @@
 
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     # disable tty hotkeys
-    stty intr undef   # Ctrl+C
-    stty quit undef   # Ctrl+\\
-    stty stop undef   # Ctrl+S
-    stty start undef  # Ctrl+Q
-    stty susp undef   # Ctrl+Z
+    stty intr undef >/dev/null 2>&1  # Ctrl+C
+    stty quit undef >/dev/null 2>&1  # Ctrl+\\
+    stty stop undef >/dev/null 2>&1  # Ctrl+S
+    stty start undef >/dev/null 2>&1 # Ctrl+Q
+    stty susp undef >/dev/null 2>&1  # Ctrl+Z
 
     # disable echo mode
-    stty -echo
+    stty -echo >/dev/null 2>&1
 
     # plymouth quit
     touch /tmp/plymouth_quit
