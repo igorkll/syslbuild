b4a7af365ce315cff83cfca5067551be6bd7799a
igorkll <frerther@gmail.com>
2026-02-23 02:23:52 +0300

    gnubox maker examples list refactor

--- gnuboxmaker_examples/electron_wayland_bashtest/resources/runshell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-systemctl --user enable pipewire pipewire-pulse wireplumber
-systemctl --user start pipewire pipewire-pulse wireplumber
-
-bash
-electron /ElectronApplication --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --no-sandbox
