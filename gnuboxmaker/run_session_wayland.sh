c59e3a6199f17df95840c98cc0f2c4b4fccd6905
igorkll <frerther@gmail.com>
2026-04-13 17:56:12 +0300

    backup

--- gnuboxmaker/run_session_wayland.sh
+++ gnuboxmaker/run_session_wayland.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+stty -echo >/dev/null 2>&1
+
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     exec weston --continue-without-input --renderer=pixman >/dev/null 2>&1
 else
