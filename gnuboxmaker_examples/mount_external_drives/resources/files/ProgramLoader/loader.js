8f1b64bef1f1001b428ea58955cd1f222f6b7eb1
igorkll <frerther@gmail.com>
2026-04-30 23:59:45 +0300

    Update loader.js

--- gnuboxmaker_examples/mount_external_drives/resources/files/ProgramLoader/loader.js
+++ gnuboxmaker_examples/mount_external_drives/resources/files/ProgramLoader/loader.js
@@ -1,12 +1,21 @@
 {
 
-const { app, BrowserWindow } = require('electron')
-const fs = require('fs')
+const { app } = require('electron')
+const afs = require('fs').promises
 const path = require('path')
 const { spawn } = require('child_process')
 
 const AUTOMOUNTS_DIR = '/automounts'
 const PROGRAM_LOADER_FILE = 'programloader'
+
+async function isFile(path) {
+    try {
+        const stats = await afs.stat(path);
+        return !stats.isDirectory()
+    } catch (err) {
+        return false;
+    }
+}
 
 let isScanning = false
 
@@ -14,12 +23,26 @@ async function refreshDisks() {
     if (isScanning) return
     isScanning = true
 
-    
+    try {
+        const entries = await afs.readdir(AUTOMOUNTS_DIR, { withFileTypes: true });
+
+        for (const entry of entries) {
+            if (entry.isDirectory()) {
+                const mountPath = path.join(AUTOMOUNTS_DIR, entry.name)
+                const programloaderPath = path.join(mountPath, PROGRAM_LOADER_FILE)
+                if (await isFile(programloaderPath)) {
+                    console.log(programloaderPath)
+                }
+            }
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
 
     isScanning = false
 }
 
 refreshDisks()
-setTimeout(refreshDisks, 1000)
+setInterval(refreshDisks, 1000)
 
 }
