06d91cc01139aa0129710c7f91e435f6a0afaf20
igorkll <frerther@gmail.com>
2026-04-30 12:13:47 +0300

    sef

---
+++ gnuboxmaker_examples/wifi/resources/files/WifiManager/main.js
@@ -0,0 +1,29 @@
+let debug = true
+
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        frame: debug,
+        fullscreen: !debug,
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            devTools: debug, // DON'T FORGET TO TURN OFF THE DAMN DEVTOOLS!
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    globalShortcut.register('F11', () => {})
+    win.loadFile(path.join(__dirname, 'main.html'))
+
+    if (debug) win.openDevTools()
+}
+
+ipcMain.on('quit-app', () => {
+    app.quit()
+})
+
+app.whenReady().then(createWindow)
