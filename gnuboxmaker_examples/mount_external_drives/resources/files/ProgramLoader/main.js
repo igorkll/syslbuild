7503aef3f67e36bc2c9374d7cc4df8b7b3ec6fcb
igorkll <frerther@gmail.com>
2026-05-01 19:55:02 +0300

    Update main.js

--- gnuboxmaker_examples/mount_external_drives/resources/files/ProgramLoader/main.js
+++ gnuboxmaker_examples/mount_external_drives/resources/files/ProgramLoader/main.js
@@ -1,6 +1,6 @@
 let debug = false
 
-const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -18,18 +18,35 @@ function createWindow () {
     })
 
     if (!debug) {
-        globalShortcut.register('F5', () => {});
-        globalShortcut.register('F11', () => {})
-        globalShortcut.register('F12', () => {})
-        globalShortcut.register('CommandOrControl+R', () => {});
-        globalShortcut.register('CommandOrControl+Shift+R', () => {});
-        globalShortcut.register('CommandOrControl+W', () => {});
-        globalShortcut.register('CommandOrControl+Q', () => {});
+        win.webContents.on('before-input-event', (event, input) => {
+            let isBlocked = false
+
+            if (input.code === 'F5' || input.code === 'F11' || input.code === 'F12') {
+                isBlocked = true
+            }
+
+            const ctrlOrCmd = input.control || input.meta
+            if (ctrlOrCmd) {
+                if (input.code === 'KeyR') {
+                    isBlocked = true
+                }
+                if (input.code === 'KeyW') {
+                    isBlocked = true
+                }
+                if (input.code === 'KeyQ') {
+                    isBlocked = true
+                }
+            }
+
+            if (isBlocked) {
+                event.preventDefault()
+            }
+        })
     }
 
     win.once('ready-to-show', () => {
-        win.show();
-    });
+        win.show()
+    })
 
     win.loadFile(path.join(__dirname, 'main.html'))
 
