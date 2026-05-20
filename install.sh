cce987ca95e1158b7d7b2513c5208f86886b26b1
igorkll <frerther@gmail.com>
2026-05-20 13:52:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,43 +4,45 @@ if [ "$EUID" -ne 0 ]; then
   exec sudo "$0" "$@"
 fi
 
-sudo apt install -y \
-  python3 wget sudo git make tar gzip \
+apt update
+
+apt install -y \
+  python3 wget git make tar gzip \
   coreutils util-linux mount \
   ncurses-bin systemd-container
-sudo apt install -y \
+apt install -y \
   e2fsprogs dosfstools btrfs-progs xfsprogs
-sudo apt install -y \
+apt install -y \
   mmdebstrap qemu-user-static binfmt-support
-sudo apt install -y \
+apt install -y \
   grub-pc-bin grub-efi-amd64-bin grub-common \
   xorriso
-sudo apt install -y \
+apt install -y \
   gcc-x86-64-linux-gnu \
   gcc-i686-linux-gnu \
   gcc-aarch64-linux-gnu \
   gcc-arm-linux-gnueabihf \
   gcc-arm-linux-gnueabi
-sudo apt install -y u-boot-tools
-sudo apt install -y arch-install-scripts
-sudo apt install -y grub-efi-ia32-bin grub-common
-sudo apt install -y device-tree-compiler
-sudo pip install json5 --break-system-packages
-sudo pip install asteval --break-system-packages
+apt install -y u-boot-tools
+apt install -y arch-install-scripts
+apt install -y grub-efi-ia32-bin grub-common
+apt install -y device-tree-compiler
+pip install json5 --break-system-packages
+pip install asteval --break-system-packages
 
 ./prepair.sh
 
 DEST="/opt/syslbuild"
-sudo mkdir -p "$DEST"
-sudo cp -r ./* "$DEST"
-sudo chmod -R 755 "$DEST"
+mkdir -p "$DEST"
+cp -r ./* "$DEST"
+chmod -R 755 "$DEST"
 
-sudo cp -f "syslbuild.py" "/usr/bin/syslbuild"
-sudo cp -f "syslbuild.desktop" "/usr/share/applications/syslbuild.desktop"
-sudo cp -f "gnuboxmaker.desktop" "/usr/share/applications/gnuboxmaker.desktop"
+cp -f "syslbuild.py" "/usr/bin/syslbuild"
+cp -f "syslbuild.desktop" "/usr/share/applications/syslbuild.desktop"
+cp -f "gnuboxmaker.desktop" "/usr/share/applications/gnuboxmaker.desktop"
 
-sudo chmod 755 "/usr/bin/syslbuild"
-sudo chmod 755 "/usr/share/applications/syslbuild.desktop"
-sudo chmod 755 "/usr/share/applications/gnuboxmaker.desktop"
+chmod 755 "/usr/bin/syslbuild"
+chmod 755 "/usr/share/applications/syslbuild.desktop"
+chmod 755 "/usr/share/applications/gnuboxmaker.desktop"
 
-sudo update-desktop-database
+update-desktop-database
