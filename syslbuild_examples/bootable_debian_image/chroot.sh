301ce6bb1b1bf2547d5a11ca193c041f4f9a3103
igorkll <frerther@gmail.com>
2026-05-27 12:16:18 +0300

    backup

--- syslbuild_examples/bootable_debian_image/chroot.sh
+++ syslbuild_examples/bootable_debian_image/chroot.sh
@@ -4,4 +4,7 @@ useradd -d /home/user -s /bin/bash user
 usermod -aG sudo user
 echo 'user:user' | chpasswd
 
+chown -R user:user /home/user
+chmod 700 /home/user
+
 echo test > /home/user/test.txt
--- syslbuild_examples/bootable_debian_image_with_graphic/chroot.sh
+++ syslbuild_examples/bootable_debian_image_with_graphic/chroot.sh
@@ -4,6 +4,9 @@ useradd -d /home/user -s /bin/bash user
 usermod -aG sudo user
 echo 'user:user' | chpasswd
 
+chown -R user:user /home/user
+chmod 700 /home/user
+
 plymouth-set-default-theme -R spinner
 
 echo test > /home/user/test.txt
