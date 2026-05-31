668e35ab0aa18d50a7341d4acc26224a70448008
igorkll <frerther@gmail.com>
2026-05-31 15:18:57 +0300

    Update system_root_init.sh

--- gnuboxmaker/system_root_init.sh
+++ gnuboxmaker/system_root_init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ -e "/bootmnt/opi_zero3" ]; then
-    modprobe /lib/modules/kernel/drivers/net/wireless/uwe5622/unisocwifi/sprdwl_ng.ko
+    modprobe /lib/modules/6.6.0-rc2-embedded-opi-zero3+/kernel/drivers/net/wireless/uwe5622/unisocwifi/sprdwl_ng.ko
 fi
