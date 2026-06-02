980475392bd0330323576f03bf981e0f1fa0120f
igorkll <frerther@gmail.com>
2026-06-02 23:19:46 +0300

    writing update system

--- gnuboxmaker/self_update.sh
+++ gnuboxmaker/self_update.sh
@@ -17,4 +17,11 @@ file_dev=$(stat -c %d "$BOOTIMAGE")
 root_dev=$(stat -c %d /)
 data_dev=$(stat -c %d /data)
 
+if [ "$file_dev" -eq "$root_dev" ]; then
+    mount -o remount,rw /
 
+    mkdir /updatescript
+    cp updatescript.sh /updatescript/updatescript.sh
+else
+    echo the update file must be located on the root partition or on the data partition
+fi
