62fb43fb76e96fc1cb983bb620b4f3b6531cef0d
igorkll <frerther@gmail.com>
2026-06-29 16:54:34 +0300

    Update chroot.sh

--- syslbuild_examples/bootable_arch_image/chroot.sh
+++ syslbuild_examples/bootable_arch_image/chroot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 useradd -d /home/user -s /bin/bash user
-usermod -aG wheel user
 echo 'user:user' | chpasswd
+
+echo "user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/user-nopasswd
+chmod 440 /etc/sudoers.d/user-nopasswd
 
 chown -R user:user /home/user
 chmod 700 /home/user
