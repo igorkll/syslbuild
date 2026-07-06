db97a1d8104e8e518adb59b9a0ce5f520f35fe9a
igorkll <frerther@gmail.com>
2026-07-06 03:13:41 +0300

    Create update_other_repos.sh

---
+++ update_other_repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -f mkbootable_readme.md ../mkbootable/README.md
+cp -f mkbootable_preview.png ../mkbootable/preview.png
+
+cp -f gnuboxmaker_readme.md ../Gnubox-Maker/README.md
+cp -f gnuboxmaker_preview.png ../Gnubox-Maker/preview.png
+
