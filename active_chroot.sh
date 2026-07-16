1ee150511652315da6e7a0fe3a3e6dc7fe8b221a
igorkll <frerther@gmail.com>
2026-07-16 21:09:18 +0300

    Update active_chroot.sh

--- active_chroot.sh
+++ active_chroot.sh
@@ -43,7 +43,7 @@ echo "active chroot: $CHROOT_PATH"
 mkdir -p "$CHROOT_PATH/opt/syslbuild"
 mkdir -p "$CHROOT_PATH/opt/home"
 mount --bind /opt/syslbuild "$CHROOT_PATH/opt/syslbuild"
-mount --bind /opt/home "$CHROOT_PATH/home"
+mount --bind /home "$CHROOT_PATH/home"
 
 if [ "$DISABLE_AUTOMOUNTS" -eq 0 ]; then
     mount --bind /dev "$CHROOT_PATH/dev"
