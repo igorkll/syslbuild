82736bcf590b1e0a03d32322f9b958f7a251670a
igorkll <frerther@gmail.com>
2026-07-18 19:31:05 +0300

    Update build.sh

--- mkbootable_examples/shellscript_orangepi_zero3/build.sh
+++ mkbootable_examples/shellscript_orangepi_zero3/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkbootable --platform orange_pi_zero3 --application shellscript.sh --packages lua,python,w3m --output shellscript_orangepi_zero3.img
+mkbootable --platform orange_pi_zero3 --application shellscript.sh --packages lua5.3,python3,w3m --output shellscript_orangepi_zero3.img
