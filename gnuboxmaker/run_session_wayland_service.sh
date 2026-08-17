92594493313a714c2e6bf0e2b59d3a30616e5d36
igorkll <frerther@gmail.com>
2026-08-17 13:26:51 +0300

    Update run_session_wayland_service.sh

--- gnuboxmaker/run_session_wayland_service.sh
+++ gnuboxmaker/run_session_wayland_service.sh
@@ -4,10 +4,7 @@ if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     export XDG_RUNTIME_DIR=/run/user/$(id -u)
     export XDG_CURRENT_DESKTOP=weston
     systemctl --user start gnubox_weston.service
-
-    while true; do
-        sleep 1
-    done
+    wait
 else
     clear
     reset
