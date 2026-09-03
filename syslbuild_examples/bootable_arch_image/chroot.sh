f9539db240574673f44c6993a29bb111ee268fd2
igorkll <frerther@gmail.com>
2026-09-03 23:42:24 +0300

    eqw

--- syslbuild_examples/bootable_arch_image/chroot.sh
+++ syslbuild_examples/bootable_arch_image/chroot.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-useradd -m -d /home/user -s /bin/bash user
+# ------------ create user
+mkdir -p /home/user
+useradd -d /home/user -s /bin/bash user
 echo 'user:user' | chpasswd
-
-echo "user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/user-nopasswd
-chmod 440 /etc/sudoers.d/user-nopasswd
-
+cp -an /etc/skel/. /home/user
 chown -R user:user /home/user
 chmod 700 /home/user
 
+# ------------ setup sudo
+echo "user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/user-nopasswd
+chmod 440 /etc/sudoers.d/user-nopasswd
+
+# ------------ example
 echo test > /home/user/test.txt
