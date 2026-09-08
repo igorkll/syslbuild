0ba64ad8d4af4fd1e015aa98323eafaae3f431cc
igorkll <frerther@gmail.com>
2026-09-08 20:32:13 +0300

    test with debian cleanup

--- gnuboxmaker/cleanup_after_firstboot.sh
+++ gnuboxmaker/cleanup_after_firstboot.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "uninitialized" > /etc/machine-id
+
+apt autoremove -y
+apt clean
+
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/cache/apt/*.bin
+rm -rf /var/log/apt/*.log
+rm -rf /var/cache/debconf/*
